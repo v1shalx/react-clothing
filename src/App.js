d0a238f4028b03d192130f7ca9100a4a229e5baa
vishal mahajan <mahajanxvishal@gmail.com>
2023-12-06 10:25:50 +0530

    updatint app with a shop page, collection preview componenet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Homepage/>
-    </div>
+   
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+   
   );
-}
+};
 
 export default App;
